811dce0331966a01e10c08c345f95ec95e3ea4df
Amine-Smahi <mohammed.amine.smahi@gmail.com>
2025-05-18 21:36:55 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ using ClusterSharp.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(2);
+    options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(1);
+    options.AllowSynchronousIO = false;
+    options.AddServerHeader = false;
+    options.Limits.MaxConcurrentConnections = 10000;
+    options.Limits.MaxConcurrentUpgradedConnections = 10000;
+});
+
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(_ => new ClusterOverviewService());
@@ -17,7 +27,15 @@ builder.Services.AddHostedService<UpdateBackgroundService>();
 
 builder.Services.AddFastEndpoints(options => options.SourceGeneratorDiscoveredTypes = []);
 
-builder.Services.AddHttpClient("ReverseProxyClient");
+// Configure HTTP client to handle connection issues
+builder.Services.AddHttpClient("ReverseProxyClient")
+    .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+    {
+        PooledConnectionLifetime = TimeSpan.FromMinutes(15),
+        KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+        KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always,
+        EnableMultipleHttp2Connections = true
+    });
 
 var app = builder.Build();
 
@@ -37,4 +55,5 @@ app.MapOpenApi();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Run with explicit host configuration
 app.Run("http://0.0.0.0:80");
